12b4b8112f5aedc45b8f08b1012790f38e5110f7
WonpyoHong <49303504+wonpyo93@users.noreply.github.com>
2022-03-04 15:00:01 +0900

    Update Constants.java

--- app/src/main/java/com/example/study_mdns/Constants.java
+++ app/src/main/java/com/example/study_mdns/Constants.java
@@ -3,5 +3,4 @@ package com.example.study_mdns;
 public class Constants {
     //public static final String SERVICE_TYPE = "_http._tcp.";
     public static final String SERVICE_TYPE = "_lg-smart-device._tcp.";
-    public static String theLog = "";
 }
